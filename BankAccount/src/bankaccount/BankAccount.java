3b7b8dd2412e6df78495ac54ebc3eac8a9401269
Lianhahaha <itsmecalcheese@gmail.com>
2025-05-08 17:57:28 +0800

    Update BankAccount.java

--- BankAccount/src/bankaccount/BankAccount.java
+++ BankAccount/src/bankaccount/BankAccount.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package bankaccount;
 
 /**
@@ -13,24 +10,24 @@ public class BankAccount {
     String accountNumber;
     double balance;
 
-    // Parameterized constructor to initialize account details
+    
     public BankAccount(String accountNumber, double balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
 
-    // Method to display account info
+    
     public void displayAccountInfo() {
         System.out.println("Account Number: " + accountNumber);
         System.out.println("Balance: $" + balance);
     }
 
-    // Method to deposit money
+    
     public void deposit(double amount) {
         balance += amount;
     }
 
-    // Method to withdraw money
+    
     public void withdraw(double amount) {
         if (amount <= balance) {
             balance -= amount;
